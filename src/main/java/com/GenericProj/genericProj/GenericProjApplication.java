7f962652c40650edc926684af36535bf82a6e4dc
divyav10 <43948786+divyav10@users.noreply.github.com>
2019-01-17 13:14:34 +0530

    initial commit

---
+++ src/main/java/com/GenericProj/genericProj/GenericProjApplication.java
@@ -0,0 +1,14 @@
+package com.GenericProj.genericProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericProjApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/GenericProj/genericProj/GenericProjApplicationTests.java
@@ -0,0 +1,17 @@
+package com.GenericProj.genericProj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GenericProjApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
